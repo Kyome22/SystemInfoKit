78b4017cbadc4789e3e68409a0442629fa5140a3
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2022-02-21 11:52:59 +0900

    round2dpを定義

---
+++ Sources/ActivityKit/Double+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by ky0me22 on 2022/02/21.
+//
+
+import Foundation
+
+extension Double {
+    var round2dp: Double {
+        return (10.0 * self).rounded() / 10.0
+    }
+}
