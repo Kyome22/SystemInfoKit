e2a3205a574c03cf9da8352e83cdafc55a228b77
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-06-15 23:16:33 +0900

    StorageInfoのtotal,available,usedの値をpublic internal(set)に変更

--- Sources/SystemInfoKit/Entity/StorageInfo.swift
+++ Sources/SystemInfoKit/Entity/StorageInfo.swift
@@ -13,9 +13,9 @@ public struct StorageInfo: SystemInfo {
     public let type = SystemInfoType.storage
     public internal(set) var value = Double.zero
     public let icon = "internaldrive"
-    public private(set) var totalValue = ByteData(value: .zero, unit: "GB")
-    public private(set) var availableValue = ByteData(value: .zero, unit: "GB")
-    public private(set) var usedValue = ByteData(value: .zero, unit: "GB")
+    public internal(set) var totalValue = ByteData(value: .zero, unit: "GB")
+    public internal(set) var availableValue = ByteData(value: .zero, unit: "GB")
+    public internal(set) var usedValue = ByteData(value: .zero, unit: "GB")
 
     public var summary: String {
         String(localized: "storage\(value)", bundle: .module)
@@ -30,26 +30,4 @@ public struct StorageInfo: SystemInfo {
     }
 
     init() {}
-
-    private func convertByteData(_ byteCount: Int64) -> ByteData {
-        let style = ByteCountFormatStyle(
-            style: .decimal,
-            allowedUnits: [.kb, .mb, .gb, .tb, .pb, .eb],
-            locale: Locale(identifier: "en_US")
-        )
-        let array = style.format(byteCount).components(separatedBy: .whitespaces)
-        return ByteData(value: Double(array[0]) ?? 0.0, unit: array[1])
-    }
-
-    mutating func setTotalValue(byteCount: Int64) {
-        totalValue = convertByteData(byteCount)
-    }
-
-    mutating func setAvailableValue(byteCount: Int64) {
-        availableValue = convertByteData(byteCount)
-    }
-
-    mutating func setUsedValue(byteCount: Int64) {
-        usedValue = convertByteData(byteCount)
-    }
 }
