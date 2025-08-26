1dda591436cb6ae731bf025f916760b5583b0e52
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-08-26 18:20:47 +0900

    Fixed tests

--- Tests/SystemInfoKitTests/PercentageTests.swift
+++ Tests/SystemInfoKitTests/PercentageTests.swift
@@ -7,39 +7,33 @@ struct PercentageTests {
     @Test(arguments: [
         .init(
             locale: Locale(languageCode: .english, languageRegion: .unitedStates),
-            expectedValue: 88.9,
             expectedDescription: "88.9%"
         ),
         .init(
             locale: Locale(languageCode: .chinese, script: .hanSimplified),
-            expectedValue: 88.9,
             expectedDescription: "88.9%"
         ),
         .init(
             locale: Locale(languageCode: .french, languageRegion: .france),
-            expectedValue: 88.9,
             expectedDescription: "88,9%"
         ),
         .init(
             locale: Locale(languageCode: .japanese, languageRegion: .japan),
-            expectedValue: 88.9,
             expectedDescription: "88.9%"
         ),
         .init(
             locale: Locale(languageCode: .korean, languageRegion: .southKorea),
-            expectedValue: 88.9,
             expectedDescription: "88.9%"
         ),
     ] as [PercentageProperty])
-    func initialize(_ property: PercentageProperty) async {
+    func initialize(_ property: PercentageProperty) {
         let sut = Percentage(rawValue: 0.8888, locale: property.locale)
-        #expect(sut.value == property.expectedValue)
+        #expect(sut.value == 88.9)
         #expect(sut.description == property.expectedDescription)
     }
 }
 
 struct PercentageProperty {
     var locale: Locale
-    var expectedValue: Double
     var expectedDescription: String
 }
